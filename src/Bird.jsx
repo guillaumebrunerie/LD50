955f6b5e0af29535b279721b0eaa7ff0e5c13e1f
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2022-04-03 23:19:50 +0200

    better hit box for birds

--- src/Bird.jsx
+++ src/Bird.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Sprite, Text } from "react-pixi-fiber/index.js";
 import { Textures, Animations } from "./Loader";
 import AnimatedSprite from "./components/AnimatedSprite";
+import Circle from "./components/Circle";
 
 // state = "flying", "standing"
 const Bird = ({ bird: { state, x, y, size, color, branch }, onClick }) => {
@@ -12,10 +13,11 @@ const Bird = ({ bird: { state, x, y, size, color, branch }, onClick }) => {
 	} else {
 		return (
 			<>
-				<Sprite texture={Textures.Birds.get(`Bird_${size}_0${color}`)} anchor={[0.5, 1]} x={x} y={y} interactive buttonMode pointerdown={onClick} />
+				<Sprite texture={Textures.Birds.get(`Bird_${size}_0${color}`)} anchor={[0.5, 1]} x={x} y={y}/>
+				<Circle x={x} y={y - 30} alpha={0.001} radius={50} interactive buttonMode pointerdown={onClick}/>
 			</>
 		);
-				// <Text text={branch.id + " " + branch.half} x={x} y={-y}/>
+		// <Text text={branch.id + " " + branch.half} x={x} y={-y}/>
 	}
 };
 
